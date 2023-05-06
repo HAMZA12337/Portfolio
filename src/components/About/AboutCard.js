aa3ded8717d5ef678c2d98d8e58ef3fa8bbbe475
Your Name <you@example.com>
2023-05-06 02:22:09 +0100

    fr

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,13 +9,17 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple"> </span>Braimi Hamza
-            from <span className="purple"> Casablanca, MOROCCO.</span>
-            <br />Software Engineering StudentI am a student so
-            
+            from <span className="purple"> Casablanca,Morocco.</span>
+            <br />Software Engineering Student and 
+            I enjoy solving problems and creating codes that live on the internet.
+                My interest in computer science started back in 2019 when I decided to try learning{" "}
+                IT-Development & Big Data Analytics— it turns out programming
+                skills are essential to achieve that, my journey with programming started from that time!
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
+          
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
